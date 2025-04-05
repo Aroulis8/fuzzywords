4d03bd16d5819f42a69a71123b4a855f97386811
Aroulis8 <136223554+Aroulis8@users.noreply.github.com>
2025-04-05 12:39:00 +0300

    Added error handling to tests for TestFuzz() and TestReverse()

--- fuzzywords_test.go
+++ fuzzywords_test.go
@@ -5,10 +5,21 @@ import (
 )
 
 func TestReverse(t *testing.T) {
-	got := Reverse("Hello")
+	got,err := Reverse("Hello")
 	want := "olleH"
 
 	if got != want {
 		t.Errorf("Wanted %q, got %q",want,got)
+	} else if err != nil {
+		t.Errorf("error is nill")
+	}
+}
+
+func TestFuzz(t *testing.T) {
+
+	_,err := Fuzz("Hello")
+
+	if err != nil {
+		t.Errorf("error is nill")
 	}
 }
