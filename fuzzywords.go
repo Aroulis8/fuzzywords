93c826037f8f6c8d8d6be047ee263ef96e7500f7
Aroulis8 <136223554+Aroulis8@users.noreply.github.com>
2025-04-07 18:41:13 +0300

    Exported RandomCharactersSlice

--- fuzzywords.go
+++ fuzzywords.go
@@ -4,9 +4,7 @@ import (
 	"math/rand"
 	"strings"
 )
-
-
- var RandomCharactersSlice = []string {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
+var RandomCharactersSlice = []string {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
 
 //Returns a random sentence made with the words from str
 func Fuzz(str string) string{
